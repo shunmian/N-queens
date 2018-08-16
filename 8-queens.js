bc55383bea6ef75bd2e4017fbfb8b63b3ea1f281
shunmian <shunmian@gmail.com>
2018-08-17 06:29:06 +0800

    convert to class

--- 8-queens.js
+++ 8-queens.js
@@ -1,53 +1,62 @@
-const N = 4
 
 function* range (max) {
-    for (let i = 0; i < max; i++ ) yield i;
+  for (let i = 0; i < max; i++ ) yield i;
 }
 
-const isConflict = (solution, i) => {
-  for(let idx of range(solution.length)){
-    const itemI = solution[idx]
-    const itemJ = idx
-    const candidateI = i
-    const candidateJ = solution.length
-    if (candidateI === itemI || // horizontal conflict
-        candidateJ === itemJ || // vertical conflict
-        Math.abs(candidateI - itemI) === Math.abs(candidateJ - itemJ) // diagonal conflict
-    ) {
-      return true
+class NQueens {
+  constructor(N) {
+    this.N = N
+    this.solutions = []
+  }
+
+  getSolutions(){
+    this.dfs()
+    return this.solutions
+  }
+
+  isConflict(solution, i){
+    for(let idx of range(solution.length)){
+      const itemI = solution[idx]
+      const itemJ = idx
+      const candidateI = i
+      const candidateJ = solution.length
+      if (candidateI === itemI || // horizontal conflict
+          candidateJ === itemJ || // vertical conflict
+          Math.abs(candidateI - itemI) === Math.abs(candidateJ - itemJ) // diagonal conflict
+      ) {
+        return true
+      }
+    }
+    return false
+  }
+
+  dfs(solution = []) {
+    if(solution.length === this.N) {
+      this.solutions.push(solution)
+      return
+    }
+    for (let i of range(this.N)) {
+      if (this.isConflict(solution, i)){
+        continue
+      }else {
+        this.dfs([...solution, i])
+      }
     }
   }
-  return false
-}
 
-
-solutions = []
-const dfs = (solution = []) => {
-  if(solution.length === N) {
-    solutions.push(solution)
-    return
-  }
-  for (let i of range(N)) {
-    if (isConflict(solution, i)){
-      continue
-    }else {
-      dfs([...solution, i])
+  toString(solution){
+    let s = ''
+    for (let j of range(this.N)){
+      for (let i of range(this.N)){
+        solution[j] === i ? s = s + ' Q' : s = s + ' *'
+      }
+      s = s + '\n'
     }
+    return s
   }
 }
 
-const toString = (solution) => {
-  let s = ''
-  for (let j of range(N)){
-    for (let i of range(N)){
-      solution[j] === i ? s = s + ' Q' : s = s + ' *'
-    }
-    s = s + '\n'
-  }
-  return s
-}
-
-dfs([], 0)
-for (let solution of solutions){
-  console.log(toString(solution))
+const nQueens = new NQueens(8)
+for (solution of nQueens.getSolutions()) {
+  console.log(nQueens.toString(solution))
 }
